d92e19b6e9d33ba1e29e2e826f34f4c1bfee44f0
Sebastian <80919128+S3bas1ian@users.noreply.github.com>
2022-01-09 21:26:03 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
   
   
   
-          public static void main(final String[] args){
+          public static void main(String[] args){
    System.out.println("Das soll kein schöner Code sein..."); 
 }
 }
